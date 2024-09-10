9b3a5f20de3cf84fa18277498e477068b4a81936
Nathan Perrine <nathan.perrine@gmail.com>
2024-09-10 15:58:42 -0400

    UnorderedList.svelte added to help with portabletext component

---
+++ Front_End/src/lib/utils/sanity/UnorderedList.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import type { BlockComponentProps } from '@portabletext/svelte';
+  export let portableText: BlockComponentProps<{bulletContext?: string}>;
+</script>
+
+{#if portableText.global.context.bulletContext === 'note'}
+  <ul class="ul-note"><slot /></ul>
+{:else if portableText.global.context.bulletContext === 'diamond'}
+  <ul class="ul-diamond"><slot /></ul>
+{:else}
+  <ul class=""><slot /></ul>
+{/if}
